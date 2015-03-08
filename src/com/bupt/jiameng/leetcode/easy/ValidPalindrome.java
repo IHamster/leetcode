ffaacb1b83d3dc1bce02736f92a1479b3ef0be5c
jiameng <jiameng@neotel.com.cn>
2015-03-08 23:25:40 +0800

    Valid Palindrome

--- src/com/bupt/jiameng/leetcode/easy/ValidPalindrome.java
+++ src/com/bupt/jiameng/leetcode/easy/ValidPalindrome.java
@@ -1,7 +1,35 @@
-package easy;
+package com.bupt.jiameng.leetcode.easy;
+
+import java.util.Stack;
 
 /**
  * Created by Administrator on 2015/3/8.
  */
 public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int length = s.length();
+        s = s.toLowerCase();
+        if(length < 2)
+            return true;
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
+                stack.push(s.charAt(i));
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+                if(s.charAt(i) == stack.pop())
+                    continue;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        ValidPalindrome v = new ValidPalindrome();
+        System.out.println(v.isPalindrome("race a car"));
+    }
 }
