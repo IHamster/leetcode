d0f3857a70a8549cce73dce67db6dd173b185753
jiameng <jiameng@neotel.com.cn>
2015-03-22 23:08:55 +0800

    Same Tree

---
+++ src/com/bupt/jiameng/leetcode/easy/SameTree.java
@@ -0,0 +1,14 @@
+package com.bupt.jiameng.leetcode.easy;
+
+/**
+ * User: Administrator
+ * Date: 2015/3/22
+ * Time: 22:50
+ */
+public class SameTree {
+    private boolean isSame = true;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+
+    }
+}
