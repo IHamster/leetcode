c199fec302cac4b2f83e7d38977054f678a0b0d7
jiameng <981266737@qq.com>
2015-03-25 23:23:18 +0800

    Remove  Duplicates from Sorted List

---
+++ src/com/bupt/jiameng/leetcode/contructs/ListNode.java
@@ -0,0 +1,14 @@
+package com.bupt.jiameng.leetcode.contructs;
+
+/**
+ * Created by jiameng on 15/3/25.
+ */
+public class ListNode {
+  public int val;
+  public ListNode next;
+
+  public ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
---
+++ src/com/bupt/jiameng/leetcode/midium/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,30 @@
+package com.bupt.jiameng.leetcode.midium;
+
+import com.bupt.jiameng.leetcode.contructs.ListNode;
+
+/**
+ * Created by jiameng on 15/3/25.
+ */
+public class RemoveDuplicatesfromSortedList {
+  public ListNode deleteDuplicates(ListNode head) {
+    if(head == null || head.next == null) return head;
+    ListNode newHead = new ListNode(0);
+    newHead.next = head;
+    ListNode tmp = newHead;
+    boolean flag = false;
+    while(tmp.next != null && tmp.next.next != null){
+      if(tmp.next.val == tmp.next.next.val){
+        flag = true;
+        tmp.next = tmp.next.next;
+      }else {
+        if(flag){
+          tmp.next = tmp.next.next.next;
+          flag = false;
+        }else{
+          tmp = tmp.next;
+        }
+      }
+    }
+    return newHead.next;
+  }
+}
