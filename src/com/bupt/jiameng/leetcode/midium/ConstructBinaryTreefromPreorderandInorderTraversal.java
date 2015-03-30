c7d83fe1db99128f2e6946d6f7919bf54c4f5f1a
jiameng <981266737@qq.com>
2015-03-30 21:25:43 +0800

    另一种方法构建二叉树

--- src/com/bupt/jiameng/leetcode/midium/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/com/bupt/jiameng/leetcode/midium/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -16,60 +16,105 @@ import com.bupt.jiameng.leetcode.contructs.TreeNode;
 
 
 public class ConstructBinaryTreefromPreorderandInorderTraversal {
-  public TreeNode buildTree(int[] preorder, int[] inorder) {
-    if(preorder == null || inorder == null) return null;
-    if(preorder.length != inorder.length) return null;
-    if(preorder.length < 1 || inorder.length < 1) return null;
+//  public TreeNode buildTree(int[] preorder, int[] inorder) {
+//    if(preorder == null || inorder == null) return null;
+//    if(preorder.length != inorder.length) return null;
+//    if(preorder.length < 1 || inorder.length < 1) return null;
+//
+//    int rootVal = preorder[0];
+//    TreeNode root = new TreeNode(rootVal);
+//    //zuo
+//    construct(root, preorder, inorder);
+//    return root;
+//  }
+//
+//  public void construct(TreeNode root, int[] preorder, int[] inorder){
+//    if(preorder == null && inorder == null){
+//      return;
+//    }
+//
+//
+//    int rootIndex = getIndex(inorder, root.val);//获取中序根节点的位置
+//    int leftNodeNum = rootIndex;//左子树节点个数
+//    int rightNodeNum = inorder.length - rootIndex - 1;//右子树节点个数
+//    int[] leftpreOrder = getArray(preorder, 1, leftNodeNum);
+//    int[] rightpreOrder = getArray(preorder, leftNodeNum + 1, preorder.length - 1);
+//    int[] leftinOrder = getArray(inorder, 0, rootIndex - 1);
+//    int[] rightinOrder = getArray(inorder, rootIndex + 1, inorder.length - 1);
+//
+//    root.left = null;
+//    root.right = null;
+//    if(leftNodeNum > 0){
+//      root.left = new TreeNode(leftpreOrder[0]);
+//    }
+//    if(rightNodeNum > 0){
+//      root.right = new TreeNode(rightpreOrder[0]);
+//    }
+//    construct(root.left, leftpreOrder, leftinOrder);
+//    construct(root.right, rightpreOrder, rightinOrder);
+//  }
+//
+//  public int[] getArray(int[] orign, int start, int end){
+//    if(start > end ) return null;
+//    int length = end - start + 1;
+//    int[] tmp = new int[length];
+//    for(int i = 0; i < length; i++){
+//      tmp[i] = orign[start + i];
+//    }
+//    return tmp;
+//  }
+//
+//  public int getIndex(int[] orign, int num){
+//    int index = -1;
+//    for(int i = 0; i < orign.length; i++){
+//      if(orign[i] == num)
+//        return i;
+//    }
+//    return index;
+//  }
 
-    int rootVal = preorder[0];
-    TreeNode root = new TreeNode(rootVal);
-    //zuo
-    construct(root, preorder, inorder);
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    if (preorder == null || inorder == null || preorder.length < 1 || inorder.length < 1)
+      return null;
+
+    int rootValue = preorder[0];
+    TreeNode root = new TreeNode(rootValue);
+    root.left = root.right = null;
+
+    if (preorder.length == 1 && inorder.length == 1) {
+      return root;
+    }
+
+    int rootInOrder = getIndex(inorder, rootValue);
+    int leftTreeLength = rootInOrder;
+    if (leftTreeLength > 0)
+      root.left = buildTree(getArray(preorder, 1, leftTreeLength), getArray(inorder, 0, leftTreeLength - 1));
+    if (leftTreeLength < preorder.length - 1)
+      root.right = buildTree(getArray(preorder, leftTreeLength + 1, preorder.length - 1), getArray(inorder, leftTreeLength + 1, inorder.length - 1));
     return root;
   }
 
-  public void construct(TreeNode root, int[] preorder, int[] inorder){
-    if(preorder == null && inorder == null){
-      return;
-    }
-
-
-    int rootIndex = getIndex(inorder, root.val);//获取中序根节点的位置
-    int leftNodeNum = rootIndex;//左子树节点个数
-    int rightNodeNum = inorder.length - rootIndex - 1;//右子树节点个数
-    int[] leftpreOrder = getArray(preorder, 1, leftNodeNum);
-    int[] rightpreOrder = getArray(preorder, leftNodeNum + 1, preorder.length - 1);
-    int[] leftinOrder = getArray(inorder, 0, rootIndex - 1);
-    int[] rightinOrder = getArray(inorder, rootIndex + 1, inorder.length - 1);
-
-    root.left = null;
-    root.right = null;
-    if(leftNodeNum > 0){
-      root.left = new TreeNode(leftpreOrder[0]);
-    }
-    if(rightNodeNum > 0){
-      root.right = new TreeNode(rightpreOrder[0]);
-    }
-    construct(root.left, leftpreOrder, leftinOrder);
-    construct(root.right, rightpreOrder, rightinOrder);
-  }
-
-  public int[] getArray(int[] orign, int start, int end){
-    if(start > end ) return null;
+  public int[] getArray(int[] orign, int start, int end) {
+    if (start > end) return null;
     int length = end - start + 1;
     int[] tmp = new int[length];
-    for(int i = 0; i < length; i++){
+    for (int i = 0; i < length; i++) {
       tmp[i] = orign[start + i];
     }
     return tmp;
   }
 
-  public int getIndex(int[] orign, int num){
+  public int getIndex(int[] orign, int num) {
     int index = -1;
-    for(int i = 0; i < orign.length; i++){
-      if(orign[i] == num)
+    for (int i = 0; i < orign.length; i++) {
+      if (orign[i] == num)
         return i;
     }
     return index;
+  }
+
+  public void construct(int[] preorder, int[] inorder) {
+//    if(preorder == null && inorder == null){
+//      return;
   }
 }
