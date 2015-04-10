5a543553828a1dbb0398c97208eaf02dc8c35fb4
jiameng <981266737@qq.com>
2015-04-10 20:30:43 +0800

    offer print1ToMaxOfNDigits

---
+++ src/com/bupt/jiameng/offer/Question12.java
@@ -0,0 +1,58 @@
+package com.bupt.jiameng.offer;
+
+/**
+ * User: jiameng
+ * Date: 15/4/9
+ * Time: 下午8:27
+ */
+public class Question12 {
+  public void print1ToMaxOfNDigits(int n){
+    if(n < 1) return;
+
+    int[] numbers = new int[n+1];
+
+    plusOne(numbers);
+
+    while(numbers[0] != 1){
+      print(numbers);
+      plusOne(numbers);
+
+    }
+  }
+
+  public void plusOne(int[] numbers){
+    int cache = 0;//保存进位
+
+    for(int i = numbers.length-1;i>=0;i--){
+      int tmp = 0;
+      if(i == numbers.length-1){
+        tmp =  numbers[i]+1;
+      }else{
+        tmp = (numbers[i]+cache);
+      }
+      numbers[i] = tmp%10;
+        cache = tmp/10;
+    }
+//    for(int i:numbers)
+//      System.out.print(i);
+//    System.out.println();
+  }
+
+  public void print(int[] numbers){
+    int i =0;
+    while(numbers[i]==0)
+      i++;
+    if(i == numbers.length){
+      System.out.println(0);
+      return;
+    }
+    while(i< numbers.length)
+      System.out.print(numbers[i++]);
+    System.out.println();
+  }
+
+  public static void main(String[] args){
+    Question12 q = new Question12();
+    q.print1ToMaxOfNDigits(6);
+  }
+}
