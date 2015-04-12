dbeb6a4a741c84f1906446e21cf3949239035e6a
jiameng <981266737@qq.com>
2015-04-12 22:52:21 +0800

    offer 打印1到最大的n位数

--- src/com/bupt/jiameng/offer/Question12.java
+++ src/com/bupt/jiameng/offer/Question12.java
@@ -6,44 +6,63 @@ package com.bupt.jiameng.offer;
  * Time: 下午8:27
  */
 public class Question12 {
+//  public void print1ToMaxOfNDigits(int n){
+//    if(n < 1) return;
+//
+//    int[] numbers = new int[n+1];
+//
+//    plusOne(numbers);
+//
+//    while(numbers[0] != 1){
+//      print(numbers);
+//      plusOne(numbers);
+//
+//    }
+//  }
+//
+//  public void plusOne(int[] numbers){
+//    int cache = 0;//保存进位
+//
+//    for(int i = numbers.length-1;i>=0;i--){
+//      int tmp = 0;
+//      if(i == numbers.length-1){
+//        tmp =  numbers[i]+1;
+//      }else{
+//        tmp = (numbers[i]+cache);
+//      }
+//      numbers[i] = tmp%10;
+//        cache = tmp/10;
+//    }
+//  }
+
   public void print1ToMaxOfNDigits(int n){
-    if(n < 1) return;
-
-    int[] numbers = new int[n+1];
-
-    plusOne(numbers);
-
-    while(numbers[0] != 1){
-      print(numbers);
-      plusOne(numbers);
-
+    if(n<=0){
+      return;
+    }
+    int[] numbers = new int[n];
+    for(int i=0;i<10;i++){
+      numbers[0]=i;
+      print1ToMaxRecursion(numbers,n,0);
     }
   }
 
-  public void plusOne(int[] numbers){
-    int cache = 0;//保存进位
-
-    for(int i = numbers.length-1;i>=0;i--){
-      int tmp = 0;
-      if(i == numbers.length-1){
-        tmp =  numbers[i]+1;
-      }else{
-        tmp = (numbers[i]+cache);
-      }
-      numbers[i] = tmp%10;
-        cache = tmp/10;
+  public void print1ToMaxRecursion(int[] numbers,int length,int index){
+    if(index ==length-1){//如果是数组的最后一个，即个位的时候，打印。
+      print(numbers);
+      return;
     }
-//    for(int i:numbers)
-//      System.out.print(i);
-//    System.out.println();
+    for(int i=0;i<10;i++){
+      numbers[index+1]=i;
+      print1ToMaxRecursion(numbers,length,index+1);
+    }
   }
 
   public void print(int[] numbers){
     int i =0;
-    while(numbers[i]==0)
+    while(i<numbers.length && numbers[i]==0)
       i++;
     if(i == numbers.length){
-      System.out.println(0);
+//      System.out.println(0);
       return;
     }
     while(i< numbers.length)
@@ -53,6 +72,6 @@ public class Question12 {
 
   public static void main(String[] args){
     Question12 q = new Question12();
-    q.print1ToMaxOfNDigits(6);
+    q.print1ToMaxOfNDigits(0);
   }
 }
