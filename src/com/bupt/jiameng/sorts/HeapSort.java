89cd1ca02c5a8419f9d3277627b590047d5b5e6a
jiameng <981266737@qq.com>
2015-05-27 00:51:35 +0800

    heap sort

---
+++ src/com/bupt/jiameng/sorts/HeapSort.java
@@ -0,0 +1,48 @@
+package com.bupt.jiameng.sorts;
+
+/**
+ * User: jiameng
+ * Date: 15/5/26
+ * Time: 下午11:57
+ */
+public class HeapSort {
+  public void heapSort(int[] nums) {
+    if (nums == null || nums.length < 1) return;
+
+    for (int i = (nums.length - 2) / 2; i >= 0; i--) {
+      heapAdjust(nums, i, nums.length - 1);
+    }
+
+    for (int i = nums.length - 1; i > 0; i--) {
+      swap(nums, 0, i);
+      heapAdjust(nums, 0, i - 1);
+    }
+  }
+
+  public void heapAdjust(int[] nums, int start, int end) {
+    int tmp = nums[start];
+    for (int i = 2 * start+1; i <= end; i = i * 2+1) {
+      if (i < end && nums[i] < nums[i + 1])
+        i++;
+      if (tmp >= nums[i])
+        break;
+      nums[start] = nums[i];
+      start = i;
+    }
+    nums[start] = tmp;
+  }
+
+  public void swap(int[] nums, int index1, int index2) {
+    int tmp = nums[index1];
+    nums[index1] = nums[index2];
+    nums[index2] = tmp;
+  }
+
+  public static void main(String[] args) {
+    HeapSort hp = new HeapSort();
+    int[] a = {5, 1, 9, 3, 7, 4, 8, 6, 2};
+    hp.heapSort(a);
+    for(int i: a)
+      System.out.printf(i+"->");
+  }
+}
