4903718187b7568f7031f479b0b4aeb766ec6afd
jiameng <981266737@qq.com>
2015-06-03 00:01:53 +0800

    heap sort

--- src/com/bupt/jiameng/sorts/HeapSort.java
+++ src/com/bupt/jiameng/sorts/HeapSort.java
@@ -7,30 +7,33 @@ package com.bupt.jiameng.sorts;
  */
 public class HeapSort {
   public void heapSort(int[] nums) {
-    if (nums == null || nums.length < 1) return;
+    if(nums == null || nums.length<1) return;
 
-    for (int i = (nums.length - 2) / 2; i >= 0; i--) {
-      heapAdjust(nums, i, nums.length - 1);
+    for(int i = nums.length/2-1;i>=0;i--){
+      heapAjust(nums,i,nums.length-1);
     }
 
-    for (int i = nums.length - 1; i > 0; i--) {
-      swap(nums, 0, i);
-      heapAdjust(nums, 0, i - 1);
+    for(int i = nums.length-1;i>=1;i--){
+      swap(nums,0,i);
+      heapAjust(nums,0,i-1);
     }
   }
 
-  public void heapAdjust(int[] nums, int start, int end) {
+  public void heapAjust(int[] nums, int start, int end){
+
     int tmp = nums[start];
-    for (int i = 2 * start+1; i <= end; i = i * 2+1) {
-      if (i < end && nums[i] < nums[i + 1])
+    for(int i = start*2+1;i<=end;i=i*2+1){
+      if(i<end && nums[i]<nums[i+1])
         i++;
-      if (tmp >= nums[i])
+      if(tmp>=nums[i])
         break;
       nums[start] = nums[i];
       start = i;
     }
     nums[start] = tmp;
+
   }
+
 
   public void swap(int[] nums, int index1, int index2) {
     int tmp = nums[index1];
